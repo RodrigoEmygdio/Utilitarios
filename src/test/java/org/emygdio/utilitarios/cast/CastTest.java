8535efcb43c3eabcb995d1872bfdd46cd67db472
Rodrigo Emygdio <rodrigo.emygdio@gmail.com>
2020-02-18 10:11:09 -0300

    Update CastTest.java
    WIP: Fazer as mudanças para ficar mais utilizável para os tipos numéricos

--- src/test/java/org/emygdio/utilitarios/cast/CastTest.java
+++ src/test/java/org/emygdio/utilitarios/cast/CastTest.java
@@ -13,33 +13,70 @@ class CastTest {
 
     Optional valor = Optional.empty();
 
-    @Test
-    public void devera_retornar_valor_com_cast_informado() throws IllegalAccessException, InstantiationException, InvocationTargetException {
-        Cast<Integer> objectCast = Cast.newInstance();
+     @Test
+    public void devera_retornar_valor_com_cast_informado() throws BancoobException {
+        ParametroCastUtil<Integer> objectCast = ParametroCastUtil.newInstance();
         Number retorno = objectCast.castValor(valor, Integer.class);
         assertThat(retorno, instanceOf(Integer.class));
     }
 
     @Test
-    void devera_retronar_valores_padrao_para_Numbers() throws IllegalAccessException, InstantiationException, InvocationTargetException {
-        Cast<Integer> objectCast = Cast.newInstance();
+    public void devera_retronar_valores_padrao_para_Numbers() throws BancoobException {
+        ParametroCastUtil<Integer> objectCast = ParametroCastUtil.newInstance();
         Number retorno = objectCast.castValor(valor, Integer.class);
         assertThat(retorno, is(equalTo(0)));
 
     }
 
     @Test
-    void devera_retornar_valor_padrao_para_Strings() throws IllegalAccessException, InstantiationException, InvocationTargetException {
-        Cast<String> objectCast = Cast.newInstance();
+    public void devera_retornar_valor_padrao_para_Strings() throws BancoobException {
+        ParametroCastUtil<String> objectCast = ParametroCastUtil.newInstance();
         String retorno = objectCast.castValor(valor, String.class);
-        assertThat(retorno, is(Matchers.emptyString()));
+        assertThat(retorno.length(), is(equalTo(0)));
     }
 
     @Test
-    void devera_retornar_valor_padrao_para_Long() throws IllegalAccessException, InstantiationException, InvocationTargetException {
-        Cast<Long> objectCast = Cast.newInstance();
+    public void devera_retornar_valor_padrao_para_Long() throws BancoobException {
+        ParametroCastUtil<Long> objectCast = ParametroCastUtil.newInstance();
         Long retorno = objectCast.castValor(valor, Long.class);
-        assertThat(retorno, is(equalTo(0l)));
+        assertThat(retorno, is(equalTo(0L)));
+    }
+
+    @Test
+    public void devera_retornar_valor_padrao_para_Number() throws BancoobException {
+       ParametroCastUtil<Number>  obj = ParametroCastUtil.newInstance();
+       Number retorno = obj.castValor(valor, Number.class);
+       assertThat(retorno, is(equalTo(0)));
+    }
+
+    @Test(expected = NegocioException.class)
+    public void deverar_fazer_cast_string_para_Number() throws BancoobException {
+        Object valor = "1";
+        ParametroCastUtil<Number> castUtil = ParametroCastUtil.newInstance();
+        Number retorno = castUtil.castValor(Optional.of(valor), Number.class) ;
+    }
+
+    @Test
+    public  void deverar_fazer_cast_string_para_Integer() throws BancoobException {
+        Object valor = "1";
+        ParametroCastUtil<Integer> castUtil = ParametroCastUtil.newInstance();
+        Integer retorno = castUtil.castValor(Optional.of(valor), Integer.class);
+        assertThat(retorno, is(equalTo(1)));
+
+    }
+
+    @Test
+    public void deverar_retornar_valor_padrao_para_Date() throws BancoobException {
+        ParametroCastUtil<Date> objetoCast = ParametroCastUtil.newInstance();
+        Date retorno = objetoCast.castValor(valor, Date.class);
+        assertThat(retorno, is(nullValue()));
+    }
+
+    @Test
+    public void devera_retronar_valor_padrao_para_Boolean() throws BancoobException {
+        ParametroCastUtil<Boolean> objetoCast = ParametroCastUtil.newInstance();
+        Boolean retorno = objetoCast.castValor(valor, Boolean.class);
+       assertFalse(retorno);
     }
 
 
